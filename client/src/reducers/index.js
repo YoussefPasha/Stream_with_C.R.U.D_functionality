67c251056681dea99903b6b259a4e5a9f3f58c9f
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-11-13 23:10:50 +0200

    done crete reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import { reducer } from "redux-form";
+import streamReducer from "./streamReducer";
 export default combineReducers({
   auth: authReducer,
   form: reducer,
+  streams: streamReducer,
 });
